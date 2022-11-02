921385dc2a1d5879ce23d8e8476f926d2c9802e9
SlackerEva <lesiv_anna@mail.ru>
2022-11-02 15:09:51 +0300

    added sceleton for autorization

---
+++ front/src/views/Login.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="max-w-screen-sm mx-auto px-4 py-10">
+    <div 
+      v-if="errorMsg"
+      class="mb-10 p-4 rounded-md bg-light-grey shadow-lg"
+    >
+      <p class="text-red-500">
+        {{ errorMsg }}
+      </p>
+    </div>
+    <!--   -->
+    <form
+      @submit.prevent="login"
+      class="p-8 flex flex-col bg-light-grey rounded-md shadow-lg"
+    >
+      <h1 class="text-3xl text-light-green mb-4">
+        Login
+      </h1>
+
+      <div class="flex flex-col mb-2">
+        <label 
+          for="email" 
+          class="mb-1 text-light-green"
+        >
+          Email
+        </label>
+        <input
+          type="text"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="email"
+          v-model="email"
+        /><!--  -->
+      </div>
+
+      <div class="flex flex-col mb-2">
+        <label 
+          for="password" 
+          class="mb-1 text-light-green"
+        >
+          Password
+        </label>
+        <input
+          type="password"
+          required
+          class="p-2 text-gray-500 focus:outline-none"
+          id="password"
+          v-model="password"
+        /><!--  -->
+      </div>
+
+      <button
+        type="submit"
+        class="mt-6 py-2 px-6 rounded-sm 
+        text-white bg-light-green duration-500 
+        border-solid border-2 border-transparent 
+        hover:border-light-green hover:bg-white hover:text-light-green"
+      >
+        Login
+      </button>
+
+      <router-link class="mt-6 text-center" :to="'/'">
+        У вас нет аккаунта? 
+        <span class="text-light-green">
+          Зарегестрироваться
+        </span>
+      </router-link> 
+    </form>
+  </div>
+</template>
+
+<script>
+  import { ref } from "vue";
+ // import { useRouter } from "vue-router";
+ //:to="{ name: 'user_register' }"
+  export default {
+    name: "user_login",
+    setup() {
+      const email = ref(null);
+      const password = ref(null);
+      const errorMsg = ref(null);
+      return { email, password, errorMsg, };
+    },
+  };
+</script>
